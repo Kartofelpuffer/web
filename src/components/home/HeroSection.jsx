6f30d0d8489a40d5bda89fefc0783a88a4d1e5b1
Kartofelpuffer <128917479+Kartofelpuffer@users.noreply.github.com>
2026-01-30 07:57:42 -0600

    Add files via upload

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -4,94 +4,10 @@ import { Phone } from 'lucide-react';
 
 export default function HeroSection() {
   useEffect(() => {
-    // Load Zoho form script
+    // Load Calendly widget script
     const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.innerHTML = `
-      (function() {
-        try{
-          var f = document.createElement("iframe");
-          var ifrmSrc = 'https://forms.zohopublic.com/summitautocaretxgm1/form/PersonalInformationForm/formperma/wfYbJVEC9sfk6il1Rk2oLrIjPP9sTXCVzB29M2uLXqE?zf_rszfm=1';
-          
-          try{
-            if ( typeof ZFAdvLead != "undefined" && typeof zfutm_zfAdvLead != "undefined" ) {
-              for( var prmIdx = 0 ; prmIdx < ZFAdvLead.utmPNameArr.length ; prmIdx ++ ) {
-                var utmPm = ZFAdvLead.utmPNameArr[ prmIdx ];
-                utmPm = ( ZFAdvLead.isSameDomian && ( ZFAdvLead.utmcustPNameArr.indexOf(utmPm) == -1 ) ) ? "zf_" + utmPm : utmPm;
-                var utmVal = zfutm_zfAdvLead.zfautm_gC_enc( ZFAdvLead.utmPNameArr[ prmIdx ] );
-                if ( typeof utmVal !== "undefined" ) {
-                  if ( utmVal != "" ) {
-                    if(ifrmSrc.indexOf('?') > 0){
-                      ifrmSrc = ifrmSrc+'&'+utmPm+'='+utmVal;
-                    }else{
-                      ifrmSrc = ifrmSrc+'?'+utmPm+'='+utmVal;
-                    }
-                  }
-                }
-              }
-            }
-            if ( typeof ZFLead !== "undefined" && typeof zfutm_zfLead !== "undefined" ) {
-              for( var prmIdx = 0 ; prmIdx < ZFLead.utmPNameArr.length ; prmIdx ++ ) {
-                var utmPm = ZFLead.utmPNameArr[ prmIdx ];
-                var utmVal = zfutm_zfLead.zfutm_gC_enc( ZFLead.utmPNameArr[ prmIdx ] );
-                if ( typeof utmVal !== "undefined" ) {
-                  if ( utmVal != "" ){
-                    if(ifrmSrc.indexOf('?') > 0){
-                      ifrmSrc = ifrmSrc+'&'+utmPm+'='+utmVal;
-                    }else{
-                      ifrmSrc = ifrmSrc+'?'+utmPm+'='+utmVal;
-                    }
-                  }
-                }
-              }
-            }
-          }catch(e){}
-          
-          f.src = ifrmSrc;
-          f.style.border="none";
-          f.style.height="980px";
-          f.style.width="100%";
-          f.style.transition="all 0.5s ease";
-          f.setAttribute("aria-label", 'Get a FREE Quote Today');
-          
-          var d = document.getElementById("zf_div_wfYbJVEC9sfk6il1Rk2oLrIjPP9sTXCVzB29M2uLXqE");
-          if(d) {
-            while(d.firstChild) {
-              d.removeChild(d.firstChild);
-            }
-            d.appendChild(f);
-          }
-          window.addEventListener('message', function (){
-            var evntData = event.data;
-            if( evntData && evntData.constructor == String ){
-              var zf_ifrm_data = evntData.split("|");
-              if ( zf_ifrm_data.length == 2 || zf_ifrm_data.length == 3 ) {
-                var zf_perma = zf_ifrm_data[0];
-                var zf_ifrm_ht_nw = ( parseInt(zf_ifrm_data[1], 10) + 15 ) + "px";
-                var iframe = document.getElementById("zf_div_wfYbJVEC9sfk6il1Rk2oLrIjPP9sTXCVzB29M2uLXqE").getElementsByTagName("iframe")[0];
-                if ( (iframe.src).indexOf('formperma') > 0 && (iframe.src).indexOf(zf_perma) > 0 ) {
-                  var prevIframeHeight = iframe.style.height;
-                  var zf_tout = false;
-                  if( zf_ifrm_data.length == 3 ) {
-                    iframe.scrollIntoView();
-                    zf_tout = true;
-                  }
-                  if ( prevIframeHeight != zf_ifrm_ht_nw ) {
-                    if( zf_tout ) {
-                      setTimeout(function(){
-                        iframe.style.height = zf_ifrm_ht_nw;
-                      },500);
-                    } else {
-                      iframe.style.height = zf_ifrm_ht_nw;
-                    }
-                  }
-                }
-              }
-            }
-          }, false);
-        }catch(e){}
-      })();
-    `;
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
     document.body.appendChild(script);
 
     return () => {
@@ -150,7 +66,7 @@ export default function HeroSection() {
             transition={{ duration: 0.8, delay: 0.2 }}
             className="flex items-center justify-center"
           >
-            <div id="zf_div_wfYbJVEC9sfk6il1Rk2oLrIjPP9sTXCVzB29M2uLXqE" className="w-full"></div>
+            <div className="calendly-inline-widget w-full" data-url="https://calendly.com/summitautocaretx/new-meeting?primary_color=003057" style={{ minWidth: '320px', height: '700px' }}></div>
           </motion.div>
         </div>
       </div>
