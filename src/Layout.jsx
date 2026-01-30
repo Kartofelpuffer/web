5db94e140f9abaf91bd239329cd9ce959f560f61
Kartofelpuffer <128917479+Kartofelpuffer@users.noreply.github.com>
2026-01-30 15:13:54 -0600

    Add files via upload

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,13 +3,28 @@ import React, { useEffect } from 'react';
 export default function Layout({ children, currentPageName }) {
   useEffect(() => {
     // Add Google Tag Manager script to head
-    const script = document.createElement('script');
-    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    const gtmScript = document.createElement('script');
+    gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-569H9KCZ');`;
-    document.head.insertBefore(script, document.head.firstChild);
+    document.head.insertBefore(gtmScript, document.head.firstChild);
+
+    // Add Google Analytics script
+    const gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-301GVJ1MJX';
+    document.head.insertBefore(gtagScript, document.head.firstChild);
+
+    const gtagConfigScript = document.createElement('script');
+    gtagConfigScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-301GVJ1MJX');
+    `;
+    document.head.insertBefore(gtagConfigScript, document.head.firstChild);
   }, []);
 
   return (
