c5121e54d23632fc61e63cfe1d125744018fd06d
Kartofelpuffer <128917479+Kartofelpuffer@users.noreply.github.com>
2026-03-26 14:20:31 -0500

    Add iframe for Apple Music embed in PrivacyPolicy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -108,6 +108,7 @@ export default function PrivacyPolicy() {
                       ))}
                     </div>
                   </section>
+              <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/revolving-door/1779319620?i=1779319626"></iframe>
                 ))}
               </div>
             </motion.div>
